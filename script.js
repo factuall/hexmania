61b33b01a312229d28d46291a62428b4c8c2434d
Adrian Nieściur <63786641+factuall@users.noreply.github.com>
2022-08-11 23:09:23 +0200

    Introducing bugs :)

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const hexwidth = 41;
 const hexheight = 36;
-let columns = Math.round(screen.width / hexwidth);
-let rows = Math.round(screen.height / hexheight);
-columns = Math.round(screen.width / hexwidth);
-rows = Math.round(screen.height / hexheight);
+let columns = Math.round(screen.height / hexheight);
+let rows = Math.round(screen.width / hexwidth);
+columns =  Math.round(screen.height / hexheight);
+rows = Math.round(screen.width / hexwidth);
 
-
+let hueOffset = 0;
 let scene = document.getElementById("scene");
 
 function updatePattern(){
@@ -21,22 +21,26 @@ function updatePattern(){
             let offsetX = (y % 2 != 0) ? hexwidth/2 : 0;
             let leftPos = offsetX + (x * hexwidth) ;
             let topPos = y * hexheight;
-    
+            
+
             hex.style.left = leftPos + "px";
             hex.style.top = topPos + "px";
-            hex.style.filter = "invert("+ x/columns +")"
+            hex.style.filter = "invert("+ (x/columns + 0.2) +")"
             hex.id = y * 10 + x;
             hex.className = "hex";
             scene.appendChild(hex);
-    
+
     
             let top = document.createElement("img");
             top.src = "hex.svg";
             top.style.position = "absolute";
             top.style.left = leftPos + "px";
             top.style.top = -24 + topPos + "px";
-            top.id = y * 10 + x;
+            top.id = "R" + ((y * 10) + x);
             top.className = "hex";
+
+            top.style.filter = "invert(0.5) sepia(1) saturate(100) hue-rotate(" + (y * 10 + x) + "deg)"
+
             scene.appendChild(top);
     
             
@@ -46,13 +50,24 @@ function updatePattern(){
 updatePattern();
 
 
-
+//this will be for adjusting pattern to the screen size
 window.addEventListener("resize", (e) => {
     let list  = document.getElementsByClassName("hex")
     for (var i = 0; i < list.length; i++) {
         list[i].remove();
     }
-    columns = Math.round(screen.width / hexwidth);
-    rows = Math.round(screen.height / hexheight);
-    updatePattern();
+    columns =  Math.round(screen.height / hexheight);
+    rows = Math.round(screen.width / hexwidth);
+
 });
+
+//rainbow looping
+let rainbow = setInterval(() => {
+    hueOffset += 10;
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < columns; x++) {
+            document.getElementById("R" + (y * 10 + x)).style.filter = "invert(0.5) sepia(1) saturate(100) hue-rotate(" + (y * 10 + x + hueOffset) + "deg)"
+        }
+    }
+
+},500);
